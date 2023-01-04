7f5753d0f645063a27093596f0858766c08016f2
Oscar Estrada <arturostrada2020@gmail.com>
2023-01-04 05:22:33 -0600

    change background color in Alert component

--- components/alert.js
+++ components/alert.js
@@ -6,8 +6,8 @@ export default function Alert({ preview }) {
   return (
     <div
       className={cn('border-b', {
-        'bg-accent-7 border-accent-7 text-white': preview,
-        'bg-accent-1 border-accent-2': !preview,
+        'bg-black border-accent-7': preview,
+        'bg-black border-accent-2': !preview,
       })}
     >
       <Container>
